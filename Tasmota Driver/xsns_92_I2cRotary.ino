a2b42fe44c2e348c513b39eff14fe1d6ecf690e1
feedbagg <feedbagg@web.de>
2020-05-13 23:29:09 +0200

    syntax
    bracket correction mqtt

--- Tasmota Driver/xsns_92_I2cRotary.ino
+++ Tasmota Driver/xsns_92_I2cRotary.ino
@@ -54,7 +54,7 @@ void I2cRotary_Telemetry(void)
 {
   if (i2c_rotary_type) {
     uint8_t value = I2cRotary_Read();
-    snprintf_P(mqtt_data, sizeof(mqtt_data), PSTR("%s,\"I2cRotary\":%i"), mqtt_data, value);
+    snprintf_P(mqtt_data, sizeof(mqtt_data), PSTR("%s,\"I2cRotary\":{\"Rotary1\":%i}"), mqtt_data, value);
   }
 }
 
@@ -63,7 +63,7 @@ void I2cRotary_Report_Value(void)
   uint8_t newvalue = I2cRotary_Read();
   if (i2c_rotary_oldvalue != newvalue) {
     snprintf_P(mqtt_data, sizeof(mqtt_data), PSTR("{\"" D_JSON_TIME "\":\"%s\""), GetDateAndTime(DT_LOCAL).c_str());
-    snprintf_P(mqtt_data, sizeof(mqtt_data), PSTR("%s,{\"I2cRotary_Value\":%i}"), mqtt_data, newvalue);
+    snprintf_P(mqtt_data, sizeof(mqtt_data), PSTR("%s,\"I2cRotary\":{\"Rotary1\":%i}"), mqtt_data, newvalue);
     snprintf_P(mqtt_data, sizeof(mqtt_data), PSTR("%s}"), mqtt_data);
     MqttPublishPrefixTopic_P(RESULT_OR_STAT, mqtt_data);
     i2c_rotary_oldvalue = newvalue;
